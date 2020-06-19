2eefe13def5321d5f2a17575e0100a815747ad39
evnayd <vjane87@yahoo.com>
2020-06-19 17:02:32 +0300

    правки

--- js/map.js
+++ js/map.js
@@ -1,41 +1,42 @@
 'use strict';
+
+
 (function () {
   var map = document.querySelector('.map');
   var pinField = document.querySelector('.map__pins');
 
   // рисуем пины
- window.load(function (pins) {
-  var pins = window.data.getRandomPins();
-  var fragment = document.createDocumentFragment();
-  for (var i = 0; i < pins.length; i++) {
-    fragment.appendChild(window.pin.createNewPin(pins[i]));
-  pinField.appendChild(fragment);
-};
 
-  window.map = {
-    renderPins: renderPins,
-    map: map,
-    pins: pins,
-    pinField: pinField
+
+  /* window.load(function (pins) {
+    var pins = window.data.getRandomPins();
+    var fragment = document.createDocumentFragment();
+    for (var i = 0; i < pins.length; i++) {
+      fragment.appendChild(window.pin.createNewPin(pins[i]));
+    pinField.appendChild(fragment);
   };
-})();
 
+    window.map = {
+      renderPins: renderPins,
+      map: map,
+      pins: pins,
+      pinField: pinField
+    };
+  })();*/
 
-/*(function () {
-  var map = document.querySelector('.map');
-  var pinField = document.querySelector('.map__pins');
-
-  // рисуем пины
-  var pins = window.data.getRandomPins();
   var renderPins = function () {
+    var pins = window.data.getRandomPins();
     var fragment = document.createDocumentFragment();
     for (var i = 0; i < pins.length; i++) {
       fragment.appendChild(window.pin.createNewPin(pins[i]));
     }
     pinField.appendChild(fragment);
   };
+
+  // window.load(renderPins);
+
   window.map = {
     renderPins: renderPins,
     map: map
   };
-})();*/
+})();
