93f11d586785340f4c55b6b39d00a17f2d83140f
evnayd <vjane87@yahoo.com>
2020-06-22 22:09:38 +0300

    правки

--- js/backend.js
+++ js/backend.js
@@ -24,8 +24,29 @@
     xhr.send();
   };
 
+
+  var upload = function (data, onSuccess, onError) {
+    var xhr = new XMLHttpRequest();
+
+    xhr.addEventListener('load', function () {
+      if (xhr.status === 200) {
+        onSuccess(xhr.response);
+      } else {
+        onError('Cтатус ответа: ' + xhr.status + ' ' + xhr.statusText);
+      }
+    });
+
+    xhr.addEventListener('error', function () {
+      onError('Произошла ошибка соединения');
+    });
+
+    xhr.open('POST', 'https://javascript.pages.academy/keksobooking');
+    xhr.send(data);
+  };
+
   window.backend = {
-    load: load
+    load: load,
+    upload: upload
   };
 })();
 
