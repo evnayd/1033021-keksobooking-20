b3162550e52c6e3b3a010078f2d52f84aa819f39
evnayd <vjane87@yahoo.com>
2020-07-14 13:12:48 +0300

    фильтрация

--- js/filter.js
+++ js/filter.js
@@ -6,15 +6,36 @@
 
   var offerType = document.querySelector('#housing-type');
 
+  var offerRooms = document.querySelector('#housing-rooms');
+  var offerGuestsNumber = document.querySelector('#housing-guests');
+
+
   var successHandler = function (data) {
+
     var updatePins = function () {
+
       var sameTypePins = data.filter(function (it) {
         return it.offer.type === offerType.value;
       });
+
+      var sameGuestsNumPins = data.filter(function (it) {
+        return it.offer.guests == offerGuestsNumber.value;
+      });
+
+      var sameRoomsPins = data.filter(function (it) {
+        return it.offer.rooms == offerRooms.value;
+        // return it.offer.rooms === offerRooms.value;
+      });
+
+
       for (var i = 0; i <= MAX_PINS; i++) {
-        window.map.renderPins(sameTypePins);
+        // window.map.renderPins(sameTypePins);
+        //window.map.renderPins(sameRoomsPins);
+        window.map.renderPins(sameTypePins.concat(sameGuestsNumPins).concat(sameRoomsPins));
       }
     };
+
+
     window.form.formFilters.addEventListener('change', function () {
       window.card.cardCopy.remove();
       window.form.deletePins();
@@ -24,4 +45,3 @@
 
   window.backend.load('https://javascript.pages.academy/keksobooking/data', successHandler, window.backend.errorHandler);
 })();
-
