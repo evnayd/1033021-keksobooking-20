9f3afbdc14acd769689e2f24a89f16bfa14aed09
evnayd <vjane87@yahoo.com>
2020-07-15 22:47:59 +0300

    правки

--- js/filter.js
+++ js/filter.js
@@ -2,13 +2,14 @@
 
 (function () {
 
+  // var DEFAULT_VALUE = 'any';
+
   var MAX_PINS = 5;
 
   var offerType = document.querySelector('#housing-type');
 
   var offerRooms = document.querySelector('#housing-rooms');
   var offerGuestsNumber = document.querySelector('#housing-guests');
-
 
   var successHandler = function (data) {
 
@@ -24,16 +25,44 @@
 
       var sameRoomsPins = data.filter(function (it) {
         return it.offer.rooms === +offerRooms.value;
-        // return it.offer.rooms === offerRooms.value;
       });
 
+      var filteredPins = sameGuestsNumPins.concat(sameRoomsPins).concat(sameTypePins);
+      var uniquePins = filteredPins.filter(function (it, i) {
+        return filteredPins.indexOf(it) === i &&
+        it.offer.type === offerType.value &&
+        it.offer.guests === +offerGuestsNumber.value &&
+        it.offer.rooms === +offerRooms.value;
+      });
 
       for (var i = 0; i <= MAX_PINS; i++) {
-        // window.map.renderPins(sameTypePins);
-        //window.map.renderPins(sameRoomsPins);
-        window.map.renderPins(sameTypePins.concat(sameGuestsNumPins).concat(sameRoomsPins));
+        window.map.renderPins(uniquePins);
       }
     };
+
+
+    /* var successHandler = function (data) {
+
+    var updatePins = function () {
+
+      var sameTypePins = data.filter(function (it) {
+        return it.offer.type === offerType.value;
+      });
+
+      var sameGuestsNumPins = data.filter(function (it) {
+        return it.offer.guests === +offerGuestsNumber.value;
+      });
+
+      var sameRoomsPins = data.filter(function (it) {
+        return it.offer.rooms === +offerRooms.value;
+      });
+
+      for (var i = 0; i <= MAX_PINS; i++) {
+       // window.map.renderPins(sameTypePins);
+        window.map.renderPins(sameRoomsPins);
+        //window.map.renderPins(sameTypePins.concat(sameGuestsNumPins).concat(sameRoomsPins));
+      }
+    }; */
 
 
     window.form.formFilters.addEventListener('change', function () {
